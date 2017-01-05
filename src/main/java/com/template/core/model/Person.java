d17a84c4900abf7485c792ceca91341f7018da19
Andrew Shalankevych <shalankevych@gmail.com>
2017-01-05 14:40:27 +0200

    add new fields into Person class

--- src/main/java/com/template/core/model/Person.java
+++ src/main/java/com/template/core/model/Person.java
@@ -13,6 +13,8 @@ public class Person extends BaseEntity {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
+    private String firstName;
+    private String lastName;
     private String username;
     @JsonProperty
     private String email;
@@ -27,6 +29,22 @@ public class Person extends BaseEntity {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getUsername() {
